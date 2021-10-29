ebd2731e42985979f3abba7a0c062158a7a3552e
Nick Cowans <nick.cowans@gmail.com>
2021-10-29 20:36:27 +0100

    added more categories

--- scripts.js
+++ scripts.js
@@ -3,24 +3,109 @@ const scoreDisplay = document.getElementById("score");
 
 let score = 0;
 
-const genres = [
+const all_genres = [
     {
-        id: 11,
-        name: "Film"
+        id: 16,
+        name: "Board Games",
+    },
+    {
+        id: 17,
+        name: "Science & Nature",
+    },
+    {
+        id: 18,
+        name: "Computers",
+    },
+    {
+        id: 22,
+        name: "Geography",
+    },
+    {
+        id: 23,
+        name: "History",
+    },
+    {
+        id: 20,
+        name: "Mythology",
+    },
+    {
+        id: 24,
+        name: "Politics",
+    },
+    {
+        id: 19,
+        name: "Mathematics",
+    },
+    {
+        id: 9,
+        name: "General Knowledge",
     },
     {
         id: 10,
-        name: "Books"
-    },
-    {
-        id: 12,
-        name: "Music"
+        name: "Books",
     },
     {
         id: 14,
-        name: "TV"
-    }
+        name: "Television",
+    },
+    {
+        id: 21,
+        name: "Sports",
+    },
+    {
+        id: 12,
+        name: "EMusic",
+    },
+    {
+        id: 11,
+        name: "Film",
+    },
+    {
+        id: 13,
+        name: "Theatre",
+    },
+    {
+        id: 27,
+        name: "Animals",
+    },
+    {
+        id: 26,
+        name: "Celebrities",
+    },
+    {
+        id: 15,
+        name: "Video Games",
+    },
+    {
+        id: 30,
+        name: "Gadgets",
+    },
+    {
+        id: 32,
+        name: "Cartoons",
+    },
+    {
+        id: 28,
+        name: "Vehicles",
+    },
+    {
+        id: 29,
+        name: "Comics",
+    },
+    {
+        id: 25,
+        name: "Art",
+    },
 ];
+
+const numbers = Array(22).fill().map((_, index) => index + 1);
+numbers.sort(() => 22*Math.random() - 0.5);
+const indicies = numbers.slice(0, 4);
+
+console.log(numbers, indicies);
+
+var genres = [];
+indicies.forEach(i => genres.push(all_genres[i]));
 
 const levels = ["easy", "medium", "hard"];
 
@@ -30,33 +115,33 @@ function addGenre(genre) {
     column.innerHTML = genre.name;
     game.append(column);
 
-    levels.forEach(level => {
+    levels.forEach((level) => {
         const card = document.createElement("div");
         card.classList.add("card", "not-answered");
         column.append(card);
 
-        if (level === "easy"){
+        if (level === "easy") {
             card.innerHTML = 100;
         }
-        if (level === "medium"){
+        if (level === "medium") {
             card.innerHTML = 200;
         }
-        if (level === "hard"){
+        if (level === "hard") {
             card.innerHTML = 300;
         }
 
         fetch(`https://opentdb.com/api.php?amount=1&category=${genre.id}&difficulty=${level}&type=boolean`)
-        .then(response => response.json())
-        .then(data => {
-            card.setAttribute("data-question", data.results[0].question);
-            card.setAttribute("data-answer", data.results[0].correct_answer);
-            card.setAttribute("data-value", card.innerHTML);
-        })
-        .then(done => card.addEventListener("click", flipCard));
-    })
+            .then((response) => response.json())
+            .then((data) => {
+                card.setAttribute("data-question", data.results[0].question);
+                card.setAttribute("data-answer", data.results[0].correct_answer);
+                card.setAttribute("data-value", card.innerHTML);
+            })
+            .then((done) => card.addEventListener("click", flipCard));
+    });
 }
 
-genres.forEach(genre => addGenre(genre));
+genres.forEach((genre) => addGenre(genre));
 
 function flipCard() {
     this.style.fontSize = "15px";
@@ -72,26 +157,24 @@ function flipCard() {
     this.append(textDisplay, trueButton, falseButton);
 
     const allCards = Array.from(document.querySelectorAll(".card"));
-    allCards.forEach (card => card.removeEventListener("click", flipCard));
+    allCards.forEach((card) => card.removeEventListener("click", flipCard));
 }
 
 function getResult() {
-
-
-    const cardOfButton = this.parentElement
+    const cardOfButton = this.parentElement;
     if (cardOfButton.getAttribute("data-answer") === this.innerHTML) {
         score = score + parseInt(cardOfButton.getAttribute("data-value"));
         scoreDisplay.innerHTML = score;
-        cardOfButton.classList.add("correct-answer")
+        cardOfButton.classList.add("correct-answer");
         cardOfButton.removeChild(cardOfButton.lastChild);
         cardOfButton.removeChild(cardOfButton.lastChild);
     } else {
-        cardOfButton.classList.add("wrong-answer")
+        cardOfButton.classList.add("wrong-answer");
         cardOfButton.removeChild(cardOfButton.lastChild);
         cardOfButton.removeChild(cardOfButton.lastChild);
     }
 
     cardOfButton.classList.remove("not-answered");
     const allCards = Array.from(document.querySelectorAll(".not-answered"));
-    allCards.forEach (card => card.addEventListener("click", flipCard));
+    allCards.forEach((card) => card.addEventListener("click", flipCard));
 }
